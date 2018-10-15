b6066dd167314ca137ae128cf6b98a7f9982f6fd
Xavier Broadhead <xavier.broadhead@gmail.com>
2018-10-15 14:42:11 +1300

    Update Game class with parameters

--- GameWorld/src/GameWorld/Game.java
+++ GameWorld/src/GameWorld/Game.java
@@ -5,82 +5,74 @@ import java.util.HashMap;
 import javax.swing.ImageIcon;
 
 public class Game {
-  private HashMap<Integer, Map> maps;
-  private Player player;
-  private HashMap<Integer, Key> keys;
-  private HashMap<Integer, Door> doors;
-
-  /**
-   * The constructor for a Game class object.
-   * @param player The active player.
-   */
-  public Game(Player player) {
-    maps = new HashMap<Integer, Map>();
-    this.keys = new HashMap<Integer, Key>();
-    this.doors = new HashMap<Integer, Door>();
-    Position[][] map1 = {{new Position(0,0), null, new Position(2,0), new Position(3,0), null},
-      {new Position(0,1), null, new Position(2,1), null, null},
-      {new Position(0,2), new Position(1,2), new Position(2,2), null, null},
-      {null, null, null, null, null},
-      {null, null, null, null, null}};
-
-    Position[][] map2 = {{new Position(0,0), null, null, null, null},
-      {new Position(0,1), null, null, null, null},
-      {new Position(0,2), new Position(1,2), null, null, null},
-      {null, new Position(1,3), null, new Position(3,3), new Position(4,3)},
-      {null, new Position(1,4), new Position(2,4), new Position(3,4), null}};
-
-    Position[][] map3 = {{new Position(0,0), new Position(1,0), new Position(2,0), null, null},
-      {null, null, new Position(2,1), null, new Position(4,1)},
-      {null, new Position(1,2), new Position(2,2), null, new Position(4,2)},
-      {null, new Position(1,3), null, new Position(3,3), new Position(4,3)},
-      {new Position(0,4), new Position(1,4), new Position(2,4), new Position(3,4), null}};
-
-    Door door1 = new Door(true, 1, 1, 2, map1[3][0], map2[0][0], Player.Direction.NORTH);
-    Door door2 = new Door(false, 2, 2, 1, map2[0][0], map1[3][0], Player.Direction.NORTH);
-    Door door3 = new Door(true, 2, 3, 3, map2[4][3], map3[0][0], Player.Direction.EAST);
-    Door door4 = new Door(false, 3, 4, 2, map3[0][0], map2[4][3], Player.Direction.NORTH);
-    this.doors.put(1, door1);
-    this.doors.put(2, door2);
-    this.doors.put(3, door3);
-    this.doors.put(4, door4);
-
+	private HashMap<Integer, Map> maps;
+	private Player player;
+	private HashMap<Integer, Key> keys;
+	private HashMap<Integer, Door> doors;
+	
+	public Game(Player player) {
+		maps = new HashMap<Integer, Map>();
+		keys = new HashMap<Integer, Key>();
+		doors = new HashMap<Integer, Door>();
+		Position[][] map1 = {{new Position(0,0), null, new Position(2,0), new Position(3,0), null},
+							{new Position(0,1), null, new Position(2,1), null, null},
+							{new Position(0,2), new Position(1,2), new Position(2,2), null, null}
+							,{null, null, null, null, null}
+							,{null, null, null, null, null}};
+		
+		Position[][] map2 = {{new Position(0,0), null, null, null, null},
+							{new Position(0,1), null, null, null, null},
+							{new Position(0,2), new Position(1,2), null, null, null},
+							{null, new Position(1,3), null, new Position(3,3), new Position(4,3)},
+							{null, new Position(1,4), new Position(2,4), new Position(3,4), null}};
+		
+		Position[][] map3 = {{new Position(0,0), new Position(1,0), new Position(2,0), null, null},
+							{null, null, new Position(2,1), null, new Position(4,1)},
+							{null, new Position(1,2), new Position(2,2), null, new Position(4,2)},
+							{null, new Position(1,3), null, new Position(3,3), new Position (4,3)},
+							{new Position(0,4), new Position(1,4), new Position(2,4), new Position(3,4), null}};
+		
+		Door door1 = new Door(true, 1, 1, 2, map1[3][0], map2[0][0], Player.Direction.NORTH);
+		Door door2 = new Door(false, 2, 2, 1, map2[0][0], map1[3][0], Player.Direction.NORTH);
+		Door door3 = new Door(true, 2, 3, 3, map2[4][3], map3[0][0], Player.Direction.EAST);
+		Door door4 = new Door(false, 3, 4, 2, map3[0][0], map2[4][3], Player.Direction.NORTH);
+		
+		doors.put(1, door1);
+		doors.put(2, door2);
+		doors.put(3, door3);
+		doors.put(4, door4);
     
-    maps.put(1, new Map(map1));
-    maps.put(2, new Map(map2));
-    maps.put(3, new Map(map3));
+		Key key1 = new Key(1, map1[2][2], 1, "A key with no markings." , "Key", 1, door1, new ImageIcon());
+		Key key2 = new Key(1, map2[2][4], 2, "Its a key. You notice the letter Z inscribed on it.", "Key", 2, door3, new ImageIcon());
+		keys.put(1, key1);
+		keys.put(2, key2);
     
-    this.player = player;
-  }
-
-  
-  public Player getPlayer() {
-    return this.player;
-  }
-  public void setMaps(HashMap<Integer, Map> newMaps) {
-    maps = newMaps;
-  }
-
-  public HashMap<Integer, Map> getMaps() {
-    return maps;
-  }
-
-  public HashMap<Integer, Key> getKeys() {
+		maps.put(1, new Map(map1));
+		maps.put(2, new Map(map2));
+		maps.put(3, new Map(map3));
+		
+		this.player = player;
+	}
+	
+	public HashMap<Integer, Map> getMaps(){
+		return maps;
+	}
+	
+	public HashMap<Integer, Key> getKeys(){
     return keys;
   }
-
-  public HashMap<Integer, Door> getDoors() {
+	
+	public HashMap<Integer, Door> getDoors(){
     return doors;
   }
-  
-  /** Returns true if the position on the given map number is accessible.
-	 *
-	 *
+	/** Returns true if the position on the given map number is accessible
+	 * 
+	 * 
 	 * @param position - The position we are checking
 	 * @param mapNum - The map we want to check
 	 * @return - True if the position is within the accessible array stored in the given map
 	 */
-  public boolean isAccessible(Position position, Integer mapNum) {
+	public boolean isAccessible(Position position, Integer mapNum) {
 		if (position == null) return false;
 		Position[][] buffer = maps.get(mapNum).getMap();
 		int x = position.getx();
@@ -88,10 +80,10 @@ public class Game {
 		if (buffer[x][y] != null) return true;
 		else return false;
 	}
-
+	
 	/** Takes in an array of Positions, returns a 5x5 2D array of Positions correctly ordered as a map.
-	 *
-	 *
+	 * 
+	 * 
 	 * @param pos - Array of Positions we want to convert to a map
 	 * @return - 2D array of Positions
 	 */
@@ -106,10 +98,10 @@ public class Game {
 		}
 		return buffer;
 	}
-
+	
 	/** Checks how many tiles from player's current position and current direction until they hit a wall
-	 *
-	 *
+	 * 
+	 * 
 	 * @return integer representing number of tiles directly in front of player until hitting a wall
 	 */
 	public int tilesTilWall() {
@@ -250,43 +242,41 @@ public class Game {
 		}
 		return false;
 	}
-
+	
 	/**
 	 * Moves the player using WASD keys
-	 *
-	 * @param keyPressed - The key on the keyboard pressed
+	 * 
+	 * @param keyPressed - The key on the keyboard pressed 
 	 * @return - True if movement was successful
 	 */
 	public boolean movePlayer(char keyPressed) {
 		Position buffer = player.getPosition();
 		Player.Direction dir = null;
 		switch (java.lang.Character.toUpperCase(keyPressed)) {
-			case 'W':
-				dir = player.getDirection();
+			case 'W': 
+				dir = player.getDirection(); 
 				break;
-			case 'A':
+			case 'A': 
 				dir = player.getLeft();
 				player.setDirection(dir);
 				break;
-			case 'S':
-				dir = player.getDirection();
+			case 'S': 
+				dir = player.getDirection(); 
 				player.setDirection(dir);
-				if(player.moveValid(player.getBehind(), player.currentMapInteger())){
-					player.setPosition(player.requestPosition(player.getBehind()));
+				if(player.moveValid(player.getBehind(), player.currentMapInteger(), this)){
+					player.setPosition(player.requestPosition(player.getBehind(), this));
 				}
 				break;
-			case 'D':
-				dir = player.getRight();
+			case 'D': 
+				dir = player.getRight(); 
 				player.setDirection(dir);
 				break;
 		}
-
 		if (dir == null) {
 			throw new Error("Unknown input");
 		}
-
-		if (buffer == player.getPosition() && player.moveValid(dir, player.currentMapInteger())) {
-			player.setPosition(player.requestPosition(dir));
+		if (buffer == player.getPosition() && player.moveValid(dir, player.currentMapInteger(), this)) {
+			player.setPosition(player.requestPosition(dir, this));
 			return true;
 		}
 		else return false;
